fe7148c5c916d49195a6940bfb31a5ccfec40e62
sam <j2d1005@naver.com>
2023-08-09 15:23:52 +0900

    Add Linting 설정 (prettier, eslint-config-prettier, eslint-plugin-prettier, eslint-plugin-simple-import-sort, eslint-plugin-unused-imports, eslint-plugin-jsx-a11y )

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,22 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    // eslint-config-prettier, eslint-plugin-prettier 의 설정 둘다 적용 되는 extends - 다른 플러그인들과 prettier 의 겹치를 설정 제거 후 prettier 설정 적용 (맨 마지막에 적용)
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jsx-a11y', 'unused-imports', 'simple-import-sort'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'warn',
+    'unused-imports/no-unused-imports': 'warn',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    'no-unused-vars': 'warn',
   },
 }
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Options} */
+const config = {
+  "singleQuote": true,
+  "semi": true,
+  "useTabs": false,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "endOfLine": "auto"
+}
+
+export default config;
