9b54a133d586b9ac3c77be0c7303c4f1a18ff4f2
sam <j2d1005@naver.com>
2023-08-09 16:40:09 +0900

    Add vite config server 속성 (임시)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    open: true,
+    port: 6060, // 임시
+  },
 });
