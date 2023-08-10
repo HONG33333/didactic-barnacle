c2fee0aa1b5ac3c2798b6b74b9ade82b4c9e7144
sam <j2d1005@naver.com>
2023-08-10 14:37:24 +0900

    Update 절대경로에서 public 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import './App.css';
 
 import reactLogo from '@assets/react.svg';
 import TestButton from '@components/TestButton';
-import viteLogo from '@public/vite.svg';
 import { useState } from 'react';
+
+import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
