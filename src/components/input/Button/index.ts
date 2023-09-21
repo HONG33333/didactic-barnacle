2088b9c810fea791d2322b2050ae545fe795e37c
sam <j2d1005@naver.com>
2023-09-21 11:38:17 +0900

    Update Button style 요소 export

--- src/components/input/Button/index.ts
+++ src/components/input/Button/index.ts
@@ -1,2 +1,4 @@
 export type { ButtonProps } from './Button';
 export { Button } from './Button';
+export type { ButtonVariants } from './button.css';
+export { button as buttonRecipe } from './button.css';
